a4b56661ad06888efd358d61f8dc6020795e7b9d
Reyhaneh_Mahmoudi <74957886+Mahmoudi1993@users.noreply.github.com>
2024-06-19 00:28:33 +0330

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,7 @@
 pip install -U torch==1.5 torchvision==0.6 -f https://download.pytorch.org/whl/cu101/torch_stable.html
 pip install cython pyyaml==5.1
-pip install -U 
+# Download
+wget -c https://figshare.com/ndownloader/files/29138334?private_link=0c1dfc3be66eb622cf85
+# xtract:
+python -m zipfile -e cifar10-raw-images.zip ./data
 pip install dominate==2.4.0
