fff91465396893fea886b33c8b3dda222465b452
Reyhaneh_Mahmoudi <74957886+Mahmoudi1993@users.noreply.github.com>
2024-06-20 13:31:23 +0330

    Update CIFAR10.sh

--- data/CIFAR10.sh
+++ data/CIFAR10.sh
@@ -1,7 +1,12 @@
 # dataset cifar10
-Download:
+# Download:
+# cifar10_raw
 wget -c https://figshare.com/ndownloader/files/29138334?private_link=0c1dfc3be66eb622cf85
-xtract:
+# cifar-10-python
+wget -c https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+# xtract:
 python -m zipfile -e cifar10-raw-images.zip ./data
-or
-!unzip ./data/cifar10-raw-images.zip
+python -m zipfile -e cifar-10-python.tar.gz ./data
+# or
+#!unzip ./data/cifar10-raw-images.zip
+#!unzip ./data/cifar-10-python.tar.gz
